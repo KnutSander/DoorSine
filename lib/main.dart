998e9e5cbbaef4f6ffa1b4952077cc125005a9f4
kb19109 <kb19109@essex.ac.uk>
2021-10-11 17:15:17 +0100

    Made the thang look decent

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,22 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+
+// TODO: Make app more adaptive, less set sizes
+
 class _HomePageState extends State<HomePage> {
+
+  bool busy = false;
+  bool inOffice = false;
+
+  // Misc properties for the labels
+  MaterialStateProperty<Color> disabled = MaterialStateProperty.all<Color>(Colors.grey);
+  MaterialStateProperty<Size> minLabelSize = MaterialStateProperty.all<Size>(const Size(450.0, 100.0));
+  TextStyle labelText = const TextStyle(fontSize: 69.0, color: Colors.white);
+
+  // Misc properties of the intractable buttons
+  TextStyle buttonText = const TextStyle(fontSize: 42.0);
+  MaterialStateProperty<Size> minButtonSize = MaterialStateProperty.all<Size>(const Size(370.0, 70.0));
 
   @override
   Widget build(BuildContext context) {
@@ -41,6 +56,11 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            const Image(
+              image: NetworkImage('https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg'),
+              height: 200.0,
+              width: 200.0,
+            ),
             const Text(
               'Staff Name',
               style: TextStyle(fontSize: 80.0),
@@ -49,44 +69,121 @@ class _HomePageState extends State<HomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Column( // Busy/Available
-                  children: const <Widget>[
-                    ElevatedButton( // Available
-                      onPressed:  null,
-                      child: Text('Available'),
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton( // Available
+                        onPressed:  null,
+                        child: Text(
+                          'Available',
+                          style: labelText,
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor: busy
+                              ? disabled
+                              : MaterialStateProperty.all<Color>(Colors.green),
+                          minimumSize: minLabelSize,
+                        ),
+                      ),
                     ),
-                    ElevatedButton( // Busy
-                      onPressed: null,
-                      child: Text('Busy'),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton( // Busy
+                        onPressed: null,
+                        child: Text(
+                          'Busy',
+                          style: labelText,
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor: busy
+                              ? MaterialStateProperty.all<Color>(Colors.red)
+                              : disabled,
+                          minimumSize: minLabelSize,
+                        ),
+                      ),
                     ),
                   ],
                 ),
                 Column( // In/Out of Office
-                  children: const <Widget>[
-                    ElevatedButton( // In Office
-                      onPressed: null,
-                      child: Text('In Office'),
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton( // In Office
+                        onPressed: null,
+                        child: Text(
+                          'In Office',
+                          style: labelText,
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor: inOffice
+                              ? MaterialStateProperty.all<Color>(Colors.blue)
+                              : disabled,
+                          minimumSize: minLabelSize,
+                        ),
+                      ),
                     ),
-                    ElevatedButton( // Out of Office
-                      onPressed: null,
-                      child: Text('Out of Office'),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton( // Out of Office
+                        onPressed: null,
+                        child: Text(
+                          'Out of Office',
+                          style: labelText,
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor: inOffice
+                              ? disabled
+                              : MaterialStateProperty.all<Color>(Colors.orange),
+                          minimumSize: minLabelSize,
+                        ),
+                      ),
                     ),
                   ],
                 ),
               ],
             ),
             Column( // This column will contain the options presented
+              //TODO: Add functionality to the buttons
+              //TODO: Make buttons visible based on availability and location
               children: <Widget>[
-                OutlinedButton(
-                  onPressed: () {},
-                  child: const Text('Message'),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Message',
+                      style: buttonText,
+                    ),
+                    style: ButtonStyle(
+                      minimumSize: minButtonSize,
+                    ),
+                  ),
                 ),
-                OutlinedButton(
-                  onPressed: () {},
-                  child: const Text('Schedule Meeting'),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Schedule Meeting',
+                      style: buttonText,
+                    ),
+                    style: ButtonStyle(
+                      minimumSize: minButtonSize,
+                    ),
+                  ),
                 ),
-                OutlinedButton(
-                  onPressed: () {},
-                  child: const Text('Staff Info'),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Staff Info',
+                      style: buttonText,
+                    ),
+                    style: ButtonStyle(
+                      minimumSize: minButtonSize,
+                    ),
+                  ),
                 ),
               ],
             ),
