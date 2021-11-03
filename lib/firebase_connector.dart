ca02fd3e263de68f8a32d35629863ef91e603bb4
kb19109 <kb19109@essex.ac.uk>
2021-11-03 23:07:21 +0000

    updated upload method to upload all data

--- lib/firebase_connector.dart
+++ lib/firebase_connector.dart
@@ -17,19 +17,16 @@ class FirebaseConnector extends ChangeNotifier {
     notifyListeners();
   }
 
-  void uploadData(Lecturer lecturer){
+  static void uploadData(Lecturer lecturer){
     FirebaseFirestore.instance.collection('lecturer').doc(lecturer.email).set(<String, dynamic>{
       'id': lecturer.id,
       'title': lecturer.title,
       'name': lecturer.name,
       'email': lecturer.email,
+      'picture link': lecturer.pictureLink,
       'office hours': lecturer.officeHours,
       'busy': lecturer.busy,
       'out of office': lecturer.outOfOffice,
     });
-  }
-
-  Future<DocumentSnapshot<Map<String, dynamic>>> getData(){
-    return FirebaseFirestore.instance.collection('lecturer').doc('sStt8bGI80yjeIjagxIS').get();
   }
 }
