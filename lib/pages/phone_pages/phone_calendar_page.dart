99a59569c47c1ababdb432d836d5ee204701725e
kb19109 <kb19109@essex.ac.uk>
2022-01-23 14:17:15 +0000

    phone calendar now displays outlook events

--- lib/pages/phone_pages/phone_calendar_page.dart
+++ lib/pages/phone_pages/phone_calendar_page.dart
@@ -22,10 +22,7 @@ class PhoneCalendarPage extends StatefulWidget {
 class _PhoneCalendarPageState extends State<PhoneCalendarPage> {
   late DeviceCalendarPlugin _deviceCalendarPlugin;
   List<Calendar> _calendars = [];
-  List<Calendar> get _writableCals =>
-      _calendars.where((c) => c.isReadOnly == false).toList();
-  List<Calendar> get _readOnlyCals =>
-      _calendars.where((c) => c.isReadOnly == true).toList();
+  List<Event> _events = [];
 
   _PhoneCalendarPageState() {
     _deviceCalendarPlugin = DeviceCalendarPlugin();
@@ -53,99 +50,155 @@ class _PhoneCalendarPageState extends State<PhoneCalendarPage> {
       final calResults = await _deviceCalendarPlugin.retrieveCalendars();
       setState(() {
         _calendars = calResults.data as List<Calendar>;
+        _getEvents();
       });
     } on PlatformException catch (e) {
       print(e);
     }
   }
 
+  void _getEvents() async {
+    var outlookCalendar;
+    for(Calendar cal in _calendars){
+      if(cal.name == 'Calendar'){
+        outlookCalendar = cal;
+        break;
+      }
+    }
+
+    if(outlookCalendar != null){
+      // Should get all events this year
+      var events = await _deviceCalendarPlugin.retrieveEvents(outlookCalendar.id,
+          RetrieveEventsParams(startDate: DateTime(DateTime.now().year), endDate: DateTime(DateTime.now().year+1)));
+
+      // Get the data and try adding them to the _events list
+      UnmodifiableListView<Event>? eventList = events.data;
+      if(eventList != null){
+        for(Event event in eventList){
+          _events.add(event);
+        }
+      }
+    } else {
+      print('No Outlook Calendar');
+    }
+  }
+
+  CDS _getEventsDataSource() {
+    List<Appointment> appointments = <Appointment>[];
+    for(Event event in _events){
+      appointments.add(
+        Appointment(
+          color: const Color.fromRGBO(205, 61, 50, 1),
+            subject: event.title.toString(),
+            startTime: DateTime(event.start!.year, event.start!.month, event.start!.day, event.start!.hour, event.start!.minute),
+            endTime: DateTime(event.end!.year, event.end!.month, event.end!.day, event.end!.hour, event.end!.minute))
+      );
+    }
+
+    return CDS(appointments);
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return Scaffold(
-    //   body: Container(
-    //     child: SfCalendar(
-    //       view: CalendarView.week,
-    //       showNavigationArrow: true,
-    //       showWeekNumber: true,
-    //     ),
-    //   ),
-    // );
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: ListView.builder(
-              itemCount: _calendars.length,
-              itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  key: Key(_calendars[index].isReadOnly == true
-                      ? 'readOnlyCalendar${_readOnlyCals.indexWhere((c) => c.id == _calendars[index].id)}'
-                      : 'writableCalendar${_writableCals.indexWhere((c) => c.id == _calendars[index].id)}'),
-                  onTap: () async {
-                    if(_calendars[index].isReadOnly == false){
-                      // Creates a new event at the given date and time
-                      Location location = Location('Somewhere', [0], [0], [TimeZone.UTC]);
-                      TZDateTime start = TZDateTime(location, 2022, 1, 23, 12, 30);
-                      TZDateTime end = TZDateTime(location, 2022, 1, 23, 13);
-                      Event event = Event(_calendars[index].id, title: 'ABBA', start: start, end: end);
-                      var eventResult = await _deviceCalendarPlugin.createOrUpdateEvent(event);
-                      if(eventResult!.isSuccess && eventResult.data!.isNotEmpty){
-                        print('Success!');
-                      } else {
-                        print(eventResult.data);
-                      }
-
-                      // var events = await _deviceCalendarPlugin.retrieveEvents(_calendars[index].id,
-                      //     RetrieveEventsParams(startDate: DateTime.now(), endDate: DateTime(2023)));
-                      // UnmodifiableListView<Event>? eventList = events.data;
-                      // if(eventList != null){
-                      //   for(Event e in eventList){
-                      //     print(e.title);
-                      //   }
-                      // } else {
-                      //   print("No events to print");
-                      // }
-                    }
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: Text(
-                            _calendars[index].name!,
-                            style: Theme.of(context).textTheme.subtitle1,
-                          ),
-                        ),
-                        Container(
-                          width: 15,
-                          height: 15,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Color(_calendars[index].color!)),
-                        ),
-                        SizedBox(width: 10),
-                        Container(
-                          margin: const EdgeInsets.fromLTRB(0, 0, 5.0, 0),
-                          padding: const EdgeInsets.all(3.0),
-                          decoration: BoxDecoration(
-                              border: Border.all(color: Colors.blueAccent)),
-                          child: Text('Default'),
-                        ),
-                        Icon(_calendars[index].isReadOnly == true
-                            ? Icons.lock
-                            : Icons.lock_open)
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+      body: Container(
+        child: SfCalendar(
+          view: CalendarView.week,
+          firstDayOfWeek: 1,
+          showNavigationArrow: true,
+          showWeekNumber: true,
+          dataSource: _getEventsDataSource(),
+        ),
       ),
     );
   }
+
 }
+
+class CDS extends CalendarDataSource {
+
+  CDS(List<Appointment> appointments){
+    this.appointments = appointments;
+  }
+}
+
+// TESTING CODE, FOR REFERENCE
+// return Scaffold(
+//       body: Column(
+//         children: <Widget>[
+//           Expanded(
+//             flex: 1,
+//             child: ListView.builder(
+//               itemCount: _calendars.length,
+//               itemBuilder: (BuildContext context, int index) {
+//                 return GestureDetector(
+//                   key: Key(_calendars[index].isReadOnly == true
+//                       ? 'readOnlyCalendar${_readOnlyCals.indexWhere((c) => c.id == _calendars[index].id)}'
+//                       : 'writableCalendar${_writableCals.indexWhere((c) => c.id == _calendars[index].id)}'),
+//                   onTap: () async {
+//                     if(_calendars[index].isReadOnly == false){
+//                       // USE calendar.accountType to only place outlook events into the calendar
+//                       print(_calendars[index].accountType);
+//                       // Creates a new event at the given date and time
+//                       // Location location = Location('Somewhere', [0], [0], [TimeZone.UTC]);
+//                       // TZDateTime start = TZDateTime(location, 2022, 1, 23, 12, 30);
+//                       // TZDateTime end = TZDateTime(location, 2022, 1, 23, 13);
+//                       // Event event = Event(_calendars[index].id, title: 'ABBA', start: start, end: end);
+//                       // var eventResult = await _deviceCalendarPlugin.createOrUpdateEvent(event);
+//                       // if(eventResult!.isSuccess && eventResult.data!.isNotEmpty){
+//                       //   print('Success!');
+//                       // } else {
+//                       //   print(eventResult.data);
+//                       // }
+//
+//                       // var events = await _deviceCalendarPlugin.retrieveEvents(_calendars[index].id,
+//                       //     RetrieveEventsParams(startDate: DateTime.now(), endDate: DateTime(2023)));
+//                       // UnmodifiableListView<Event>? eventList = events.data;
+//                       // if(eventList != null){
+//                       //   for(Event e in eventList){
+//                       //     print(e.title);
+//                       //   }
+//                       // } else {
+//                       //   print("No events to print");
+//                       // }
+//                     }
+//                   },
+//                   child: Padding(
+//                     padding: const EdgeInsets.all(10.0),
+//                     child: Row(
+//                       children: [
+//                         Expanded(
+//                           flex: 1,
+//                           child: Text(
+//                             _calendars[index].name!,
+//                             style: Theme.of(context).textTheme.subtitle1,
+//                           ),
+//                         ),
+//                         Container(
+//                           width: 15,
+//                           height: 15,
+//                           decoration: BoxDecoration(
+//                               shape: BoxShape.circle,
+//                               color: Color(_calendars[index].color!)),
+//                         ),
+//                         SizedBox(width: 10),
+//                         Container(
+//                           margin: const EdgeInsets.fromLTRB(0, 0, 5.0, 0),
+//                           padding: const EdgeInsets.all(3.0),
+//                           decoration: BoxDecoration(
+//                               border: Border.all(color: Colors.blueAccent)),
+//                           child: Text('Default'),
+//                         ),
+//                         Icon(_calendars[index].isReadOnly == true
+//                             ? Icons.lock
+//                             : Icons.lock_open)
+//                       ],
+//                     ),
+//                   ),
+//                 );
+//               },
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
