b8a8601cf82a6b8e387d50d02de926a1a184b8fd
kb19109 <kb19109@essex.ac.uk>
2022-03-03 10:42:39 +0000

    started clean-up and added Clean-Up.md

---
+++ docs/Clean-Up.md
@@ -0,0 +1,36 @@
+#Clean-Up
+List of all classes and which ones I have cleaned so far.  
+The old_pages and testing folders will be removed  
+
+## lib
+main.dart x  
+main_copy.dart x  
+firebase_connector.dart  
+
+### models
+lecturer.dart  
+
+### pages
+call_page.dart  
+create_account_page.dart  
+login_page.dart  
+message_page.dart  
+phone_main.dart  
+tablet_main.dart
+
+#### phone_pages
+phone_calendar_page.dart  
+phone_call_page.dart  
+phone_home_page.dart  
+phone_messages_page.dart  
+phone_settings_page.dart  
+
+#### tablet_pages
+tablet_calendar_page.dart  
+tablet_call_page.dart  
+tablet_messages_page.dart  
+
+### widgets
+app_theme.dart  
+calendar_data_source.dart  
+message_widget.dart  
--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,18 @@
 /// Essex Capstone Project 2021/2022
 /// Last updated: 02/03/2022
 
-// Azure Client Secret Value: anG7Q~nwpPGMLthLq_9B_wY7SilSJGNQazQQC
-
+// Imports
 import 'package:capstone_project/firebase_connector.dart';
 import 'package:capstone_project/pages/login_page.dart';
-import 'package:capstone_project/pages/testing_page.dart';
 import 'package:capstone_project/widgets/app_theme.dart';
-
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_auth_oauth/firebase_auth_oauth.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'package:provider/provider.dart';
 
+// App ID used for calling
 const appID = "d73f28067efd4f2bb80d756a87326e33";
 
 // Main function
@@ -29,7 +24,7 @@ void main() {
   ));
 }
 
-// Main app class
+// Main App class
 class DoorSine extends StatelessWidget {
   DoorSine({Key? key}) : super(key: key);
 
@@ -41,28 +36,35 @@ class DoorSine extends StatelessWidget {
     return FutureBuilder(
       future: _init,
       builder: (context, snapshot) {
+
+        // Called in the MaterialApp return
         Widget loadPage() {
+          // Display error
           if (snapshot.hasError) {
             return const Scaffold(
               body: Center(child: Text('Something is wrong!')),
             );
           }
 
+          // Load LoginPage if everything is alright
           if (snapshot.connectionState == ConnectionState.done) {
             return const LoginPage();
           }
 
+          // Show a loading screen when loading
           return const Scaffold(
-           body: Center(
-             child: CircularProgressIndicator(
-               value: null,
-             ),
-           ),
+            body: Center(
+              child: CircularProgressIndicator(
+                value: null,
+              ),
+            ),
           );
         }
 
+        // Main body of the app is the MaterialApp
+        // Everything must be and is inside it
         return MaterialApp(
-          title: 'Capstone Project',
+          title: 'DoorSine',
           theme: AppTheme.appTheme,
           home: loadPage(),
         );
@@ -70,4 +72,3 @@ class DoorSine extends StatelessWidget {
     );
   }
 }
-
